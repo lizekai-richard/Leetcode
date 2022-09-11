b15290db5bd679ce2661f23eba0ea016c9cfbf45
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2022-09-11 20:59:24 +0800

    Time: 34 ms (12.93%), Space: 131.8 MB (9.68%) - LeetHub

---
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public long maxPoints(int[][] points) {
+        int n = points.length;
+        int m = points[0].length;
+        long ans = Long.MIN_VALUE;
+        long[][] dp = new long[n][m];
+        long[] forwardMax = new long[m];
+        long[] backwardMax = new long[m];
+        for (int j = 0; j < m; ++j) {
+            dp[0][j] = points[0][j];
+        }
+        for (int i = 1; i < n; ++i) {
+            forwardMax[0] = dp[i - 1][0];
+            for (int j = 1; j < m; ++j) {
+                forwardMax[j] = Math.max(forwardMax[j - 1], dp[i - 1][j] + j);
+            }
+            
+            backwardMax[m - 1] = dp[i - 1][m - 1] - (m - 1);
+            for (int j = m - 2; j >= 0; --j) {
+                backwardMax[j] = Math.max(backwardMax[j + 1], dp[i - 1][j] - j);
+            }
+            
+            for (int j = 0; j < m; ++j) {
+                dp[i][j] = Math.max(forwardMax[j] - j, backwardMax[j] + j) + points[i][j];
+            }
+        }
+        for (int j = 0; j < m; ++j) {
+            ans = Math.max(ans, dp[n - 1][j]);
+        }
+        return ans;
+    }
+}
