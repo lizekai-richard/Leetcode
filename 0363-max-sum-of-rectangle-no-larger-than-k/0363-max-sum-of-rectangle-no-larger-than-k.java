112bab0f40bc7c45fb5529d05c1ef68c9c81052d
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2022-12-08 13:13:05 +0800

    Time: 422 ms (70.21%), Space: 42.5 MB (85.68%) - LeetHub

---
+++ 0363-max-sum-of-rectangle-no-larger-than-k/0363-max-sum-of-rectangle-no-larger-than-k.java
@@ -0,0 +1,34 @@
+class Solution {
+    private int answer = Integer.MIN_VALUE;
+
+    private void updateAnswer(int[] rowSum, int k) {
+        int sum = 0;
+        TreeSet<Integer> orderedSet = new TreeSet<>();
+        orderedSet.add(0);
+        for (int s: rowSum) {
+            sum += s;
+            Integer x = orderedSet.ceiling(sum - k);
+            if (x != null) {
+                answer = Math.max(answer, sum - x);
+            }
+            orderedSet.add(sum);
+        }
+    }
+
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] rowSum = new int[n];
+
+        for (int r = 0; r < m; ++r) {
+            Arrays.fill(rowSum, 0);
+            for (int row = r; row < m; ++row) {
+                for (int col = 0; col < n; ++col) {
+                    rowSum[col] = rowSum[col] + matrix[row][col];
+                }
+                updateAnswer(rowSum, k);
+            }
+        }
+        return answer;
+    }
+}
