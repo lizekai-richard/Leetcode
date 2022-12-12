678840154333d06758f0ef56e7505514840d1fd6
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2022-12-12 11:53:45 +0800

    Time: 16 ms (67.13%), Space: 55.5 MB (23.12%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    public int[][] merge(int[][] intervals) {
+        List<int[]> mergedIntervals = new ArrayList<>();
+        
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] i1, int[] i2) {
+                return i1[1] - i2[1];
+            }
+        });
+        
+        mergedIntervals.add(intervals[intervals.length - 1]);
+        for (int i = intervals.length - 2; i >= 0; --i) {
+            // don't merge
+            if (intervals[i][1] < mergedIntervals.get(mergedIntervals.size() - 1)[0]) {
+                mergedIntervals.add(intervals[i]);
+            } else {
+                int[] newInterval = new int[2];
+                newInterval[0] = Math.min(mergedIntervals.get(mergedIntervals.size() - 1)[0], intervals[i][0]);
+                newInterval[1] = mergedIntervals.get(mergedIntervals.size() - 1)[1];
+                mergedIntervals.set(mergedIntervals.size() - 1, newInterval);
+            }
+        }
+        return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
+    }
+}
