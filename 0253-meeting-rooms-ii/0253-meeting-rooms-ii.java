7f6c2c006cbf4f885d39e85cc09c5172eb006901
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2022-12-17 10:26:06 +0800

    Time: 6 ms (91.93%), Space: 41.7 MB (99.37%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<>(){
+            @Override
+            public int compare(int[] i1, int[] i2) {
+                return i1[0] - i2[0];
+            }
+        });
+        int numRooms = 1;
+        PriorityQueue<Integer> rooms = new PriorityQueue<>();
+        rooms.add(intervals[0][1]);
+        
+        for (int i = 1; i < intervals.length; ++i) {
+            int earliestFreeRoom = rooms.peek();
+            if (earliestFreeRoom <= intervals[i][0]) {
+                rooms.poll();
+                rooms.add(intervals[i][1]);
+            } else {
+                numRooms += 1;
+                rooms.add(intervals[i][1]);
+            }
+        }
+        return numRooms;
+    }
+}
