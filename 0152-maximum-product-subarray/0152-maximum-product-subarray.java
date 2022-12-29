82a9278995f9e24c46a50b31d789a2e6bc97d469
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2022-12-29 19:44:11 +0800

    Time: 0 ms (100.00%), Space: 42.7 MB (75.35%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,19 +1,41 @@
 class Solution {
     private int[] maxProd;
     public int maxProduct(int[] nums) {
-        int n = nums.length;
-        int currentNumber = nums[0];
-        int maxSoFar = nums[0];
-        int minSoFar = nums[0];
+         int n = nums.length;
+//         int currentNumber = nums[0];
+//         int maxSoFar = nums[0];
+//         int minSoFar = nums[0];
+//         int ans = nums[0];
+        
+//         for (int i = 1; i < n; ++i) {
+//             int v1 = nums[i];
+//             int v2 = maxSoFar * nums[i];
+//             int v3 = minSoFar * nums[i];
+//             maxSoFar = Math.max(v1, Math.max(v2, v3));
+//             minSoFar = Math.min(v1, Math.min(v2, v3));
+//             ans = Math.max(ans, maxSoFar);
+//         }
+        
+//         return ans;
+        
         int ans = nums[0];
         
-        for (int i = 1; i < n; ++i) {
-            int v1 = nums[i];
-            int v2 = maxSoFar * nums[i];
-            int v3 = minSoFar * nums[i];
-            maxSoFar = Math.max(v1, Math.max(v2, v3));
-            minSoFar = Math.min(v1, Math.min(v2, v3));
-            ans = Math.max(ans, maxSoFar);
+        int product = 1;
+        for (int i = 0; i < n; ++i) {
+            product *= nums[i];
+            ans = Math.max(ans, product);
+            if (product == 0) {
+                product = 1;
+            }
+        }
+        
+        product = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            product *= nums[i];
+            ans = Math.max(ans, product);
+            if (product == 0) {
+                product = 1;
+            }
         }
         
         return ans;
