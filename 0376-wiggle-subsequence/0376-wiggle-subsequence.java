13ffe8a34a85715cfae85b2465a89f93ebd0607d
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2022-12-30 10:48:23 +0800

    Time: 0 ms (100.00%), Space: 40.3 MB (66.96%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.java
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.java
@@ -14,14 +14,24 @@ class Solution {
         int ans = 1;
         
         for (int i = 1; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] - nums[j] < 0) {
-                    wiggleSubsequence[i][0] = Math.max(wiggleSubsequence[i][0], 
-                                                  wiggleSubsequence[j][1] + 1);
-                } else if (nums[i] - nums[j] > 0) {
-                    wiggleSubsequence[i][1] = Math.max(wiggleSubsequence[i][1], 
-                                                  wiggleSubsequence[j][0] + 1);
-                }
+            // for (int j = 0; j < i; ++j) {
+            //     if (nums[i] - nums[j] < 0) {
+            //         wiggleSubsequence[i][0] = Math.max(wiggleSubsequence[i][0], 
+            //                                       wiggleSubsequence[j][1] + 1);
+            //     } else if (nums[i] - nums[j] > 0) {
+            //         wiggleSubsequence[i][1] = Math.max(wiggleSubsequence[i][1], 
+            //                                       wiggleSubsequence[j][0] + 1);
+            //     }
+            // }
+            if (nums[i] > nums[i - 1]) {
+                wiggleSubsequence[i][1] = wiggleSubsequence[i - 1][0] + 1;
+                wiggleSubsequence[i][0] = wiggleSubsequence[i - 1][0];
+            } else if (nums[i] < nums[i - 1]) {
+                wiggleSubsequence[i][0] = wiggleSubsequence[i - 1][1] + 1;
+                wiggleSubsequence[i][1] = wiggleSubsequence[i - 1][1];
+            } else {
+                wiggleSubsequence[i][1] = wiggleSubsequence[i - 1][1];
+                wiggleSubsequence[i][0] = wiggleSubsequence[i - 1][0];
             }
             ans = Math.max(ans, Math.max(wiggleSubsequence[i][0], 
                                         wiggleSubsequence[i][1]));
