8be9a66cc0c19497821bedeb34cf3943cc8356d8
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2022-12-31 17:10:22 +0800

    Time: 2 ms (99.46%), Space: 41.9 MB (98.56%) - LeetHub

--- 0265-paint-house-ii/0265-paint-house-ii.java
+++ 0265-paint-house-ii/0265-paint-house-ii.java
@@ -15,13 +15,36 @@ class Solution {
         }
         
         for (int i = 1; i < n; ++i) {
+            int minimum = Solution.INF;
+            int minIndex = -1;
+            int secondMinimum = Solution.INF;
+            int secondMinIndex = -1;
             for (int j = 0; j < k; ++j) {
-                int temp = Solution.INF;
-                for (int l = 0; l < k; ++l) {
-                    if (l == j) continue;
-                    temp = Math.min(temp, minCost[i - 1][l]);
+                if (minimum > minCost[i - 1][j]) {
+                    minimum = minCost[i - 1][j];
+                    minIndex = j;
                 }
-                minCost[i][j] = temp + costs[i][j];
+            }
+            for (int j = 0; j < k; ++j) {
+                if (secondMinimum > minCost[i - 1][j] && j != minIndex) {
+                    secondMinimum = minCost[i - 1][j];
+                    secondMinIndex = j;
+                }
+            }
+            // for (int j = 0; j < k; ++j) {
+            //     int temp = Solution.INF;
+            //     for (int l = 0; l < k; ++l) {
+            //         if (l == j) continue;
+            //         temp = Math.min(temp, minCost[i - 1][l]);
+            //     }
+            //     minCost[i][j] = temp + costs[i][j];
+            // }
+            for (int j = 0; j < k; ++j) {
+                if (j == minIndex) {
+                    minCost[i][j] = secondMinimum + costs[i][j];
+                } else {
+                    minCost[i][j] = minimum + costs[i][j];
+                }
             }
         }
         
