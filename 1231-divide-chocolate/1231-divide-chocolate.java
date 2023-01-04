3c07045999a1c768b2a71f1c0a9989c9aae5d107
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2023-01-04 11:46:22 +0800

    Time: 16 ms (13.84%), Space: 50 MB (50.28%) - LeetHub

---
+++ 1231-divide-chocolate/1231-divide-chocolate.java
@@ -0,0 +1,63 @@
+class Solution {
+    private int[] sweetness;
+    private int k;
+    private int n;
+    private int sum;
+    private int minSweetness;
+    private int ans;
+    
+    private void preprocessBoundary() {
+        sum = 0;
+        minSweetness = sweetness[0];
+        for (int i = 0; i < n; ++i) {
+            sum += sweetness[i];
+            minSweetness = Math.min(minSweetness, sweetness[i]);
+        }
+    }
+
+    private boolean check(int x) {
+        int cuts = 0;
+        int pointer = 0;
+        int currentSum = 0;
+        while (pointer < n) {
+            if (cuts == k) {
+                break;
+            }
+            if (currentSum < x) {
+                currentSum += sweetness[pointer];
+                pointer++;
+            } else {
+                cuts++;
+                currentSum = 0;
+            }
+        }
+        currentSum = 0;
+        for (int i = pointer; i < n; ++i) {
+            currentSum += sweetness[i];
+        }
+        return currentSum >= x;
+    }
+
+    private void binarySearch() {
+        int l = minSweetness, r = sum;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (check(mid)) {
+                l = mid + 1;
+                ans = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+    }
+
+    public int maximizeSweetness(int[] sweetness, int k) {
+        this.sweetness = sweetness;
+        this.k = k;
+        this.n = sweetness.length;
+
+        preprocessBoundary();
+        binarySearch();
+        return ans;
+    }
+}
