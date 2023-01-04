164e5cdfcf4b48e1360adaf6225b563469f93559
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2023-01-04 11:43:20 +0800

    Time: 42 ms (51.93%), Space: 67.5 MB (51.40%) - LeetHub

---
+++ 1229-meeting-scheduler/1229-meeting-scheduler.java
@@ -0,0 +1,67 @@
+class Solution {
+    private int compareSlot(int[] slot1, int[] slot2) {
+        if (slot1[1] <= slot2[0]) {
+            return 0;
+        } 
+        if (slot2[1] <= slot1[0]) {
+            return 1;
+        }
+        return 2;
+    }
+    
+    private int[] getIntersection(int[] slot1, int[] slot2) {
+        if (slot1[0] <= slot2[0] && slot2[1] <= slot1[1]) {
+            return slot2;
+        }
+        if (slot2[0] <= slot1[0] && slot1[1] <= slot2[1]) {
+            return slot1;
+        }
+        if (slot1[1] >= slot2[1]) {
+            return new int[]{slot1[0], slot2[1]};
+        }
+        if (slot2[1] >= slot1[1]) {
+            return new int[]{slot2[0], slot1[1]};
+        }
+        return new int[]{};
+    }
+    
+    public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
+        int n = slots1.length;
+        int m = slots2.length;
+        
+        Arrays.sort(slots1, new Comparator<>(){
+            @Override
+            public int compare(int[] x, int[] y) {
+                return x[0] - y[0];
+            }
+        });
+        Arrays.sort(slots2, new Comparator<>(){
+            @Override
+            public int compare(int[] x, int[] y) {
+                return x[0] - y[0];
+            }
+        });
+        
+        int p1 = 0, p2 = 0;
+        while (p1 < n && p2 < m) {
+            int res = compareSlot(slots1[p1], slots2[p2]);
+            if (res == 2) {
+                int[] intersection = getIntersection(slots1[p1], slots2[p2]);
+                if (intersection[1] - intersection[0] >= duration) {
+                    return Arrays.asList(new Integer[]{intersection[0], intersection[0] + duration});
+                } else {
+                    if (slots1[p1][1] > slots2[p2][1]) {
+                        p2++;
+                    } else {
+                        p1++;
+                    }
+                }
+            } else if (res == 1) {
+                p2++;
+            } else if (res == 0) {
+                p1++;
+            }
+        }
+        return new ArrayList<Integer>();
+    }
+}
