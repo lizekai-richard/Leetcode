353f57a774c7e2414c505c8fbe4c9e830de4238e
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2023-01-06 21:40:24 +0800

    Time: 10 ms (38.36%), Space: 57.7 MB (23.83%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,44 @@
+class Solution {
+    private int m;
+    private int n;
+    private int[] dx = new int[]{1, 0, -1, 0};
+    private int[] dy = new int[]{0, 1, 0, -1};
+    private char[][] grid;
+    private boolean[][] visited;
+    private int numIslands = 0;
+    
+    public void dfs(int x, int y) {
+        for (int i = 0; i < 4; ++i) {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+            if (newX >= 0 && newY >= 0 && newX < m && newY < n) {
+                if (grid[newX][newY] == '1') {
+                    grid[newX][newY] = '0';
+                    dfs(newX, newY);
+                }
+            }
+            
+        }
+    }
+    public int numIslands(char[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        this.grid = grid;
+        visited = new boolean[m][n];
+        
+        for (int i = 0; i < m; ++i) {
+            Arrays.fill(visited[i], false);
+        }
+        
+        for (int x = 0; x < m; ++x) {
+            for (int y = 0; y < n; ++y) {
+                if(grid[x][y] == '1') {
+                    dfs(x, y);
+                    numIslands++;
+                }
+            }
+        }
+        
+        return numIslands;
+    }
+}
