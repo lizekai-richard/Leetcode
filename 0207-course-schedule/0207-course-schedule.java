709c5819bbcf560a5fd16f354684bbfe6de10a05
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2023-01-09 20:41:45 +0800

    Time: 5 ms (81.83%), Space: 43 MB (70.39%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,36 +1,52 @@
 class Solution {
-    private int numCourses;
-    private int[][] prerequisites;
-    private int[] est;
+    private HashMap<Integer, List<Integer>> adjList;
+    private boolean[] checked;
+    private boolean[] path;
     
-    private boolean bellmanFord() {
-        for (int i = 0; i < numCourses; ++i) {
-            for (int j = 0; j < prerequisites.length; ++j) {
-                int[] edge = prerequisites[j];
-                int u = edge[0];
-                int v = edge[1];
-                if (est[u] > est[v] - 1) {
-                    est[u] = est[v] - 1;
-                }
+    private boolean dfs(int node) {
+        if (checked[node]) {
+            return false;
+        }
+        if (path[node]) {
+            return true;
+        }
+        if (!adjList.containsKey(node)) {
+            return false;
+        }
+        path[node] = true;
+        List<Integer> nbrNodes = adjList.get(node);
+        boolean res = false;
+        for (int u: nbrNodes) {
+            res = dfs(u);
+            if (res) {
+                break;
             }
         }
-        for (int j = 0; j < prerequisites.length; ++j) {
-            int[] edge = prerequisites[j];
-            int u = edge[0];
-            int v = edge[1];
-            if (est[u] > est[v] - 1) {
+        checked[node] = true;
+        path[node] = true;
+        return res;
+    }
+    
+    
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        adjList = new HashMap<>();
+        checked = new boolean[numCourses];
+        path = new boolean[numCourses];
+        
+        for (int i = 0; i < prerequisites.length; ++i) {
+            int[] edge = prerequisites[i];
+            if (!adjList.containsKey(edge[1])) {
+                adjList.put(edge[1], new ArrayList<>());
+            }
+            adjList.get(edge[1]).add(edge[0]);
+        }
+        
+        for (int i = 0; i < numCourses; ++i) {
+            if (dfs(i)) {
                 return false;
             }
         }
+        
         return true;
-    }
-    
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        this.numCourses = numCourses;
-        this.prerequisites = prerequisites;
-        this.est = new int[numCourses];
-        Arrays.fill(est, Integer.MAX_VALUE);
-        est[0] = 0;
-        return bellmanFord();
     }
 }
