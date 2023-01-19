74b91136bb083b63e083fc1a9b63d1d7740f0ced
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2023-01-19 19:06:05 +0800

    Time: 1 ms (86.83%), Space: 42.3 MB (61.28%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -11,17 +11,27 @@ class Solution {
         }
         return s.equals(sb.toString());
     }
+    private int gcd(int x, int y) {
+        return y == 0 ? x : gcd(y, x % y);
+    }
     public String gcdOfStrings(String str1, String str2) {
-        int n = Math.min(str1.length(), str2.length());
-        String ans = "";
-        for (int i = 0; i < n; ++i) {
-            String prefix = str1.substring(0, i + 1);
-            boolean c1 = check(str1, prefix);
-            boolean c2 = check(str2, prefix);
-            if (c1 && c2) {
-                ans = prefix;
-            }
+        // int n = Math.min(str1.length(), str2.length());
+        // String ans = "";
+        // for (int i = 0; i < n; ++i) {
+        //     String prefix = str1.substring(0, i + 1);
+        //     boolean c1 = check(str1, prefix);
+        //     boolean c2 = check(str2, prefix);
+        //     if (c1 && c2) {
+        //         ans = prefix;
+        //     }
+        // }
+        // return ans;
+        int n = str1.length();
+        int m = str2.length();
+        if (! (str1 + str2).equals(str2 + str1)) {
+            return "";
         }
-        return ans;
+        int g = gcd(n, m);
+        return str1.substring(0, g);
     }
 }
