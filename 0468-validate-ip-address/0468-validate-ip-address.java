53da53eccfce61f059dfd9c6b443eefe7b0856ff
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2023-01-19 19:34:44 +0800

    Time: 2 ms (69.41%), Space: 40.3 MB (80.34%) - LeetHub

---
+++ 0468-validate-ip-address/0468-validate-ip-address.java
@@ -0,0 +1,60 @@
+class Solution {
+    private boolean checkIPv4(String[] xs) {
+        for (String x: xs) {
+            if (x.length() > 1 && x.charAt(0) == '0') {
+                return false;
+            }
+            try {
+                int xValue = Integer.parseInt(x);
+                if (xValue < 0 || xValue > 255) {
+                    return false;
+                }
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean checkIPv6(String[] xs) {
+        for (String x: xs) {
+            if (x.length() <= 0 || x.length() > 4) {
+                return false;
+            }
+            
+            for (int i = 0; i < x.length(); ++i) {
+                char c = x.toLowerCase().charAt(i);
+                if (c >= '0' && c <= '9') {
+                    continue;
+                }
+                if (c > 'f') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public String validIPAddress(String queryIP) {
+        if (queryIP.length() == 0 || queryIP.charAt(0) == '.' || queryIP.charAt(0) == ':' || queryIP.charAt(queryIP.length() - 1) == '.' || queryIP.charAt(queryIP.length() - 1) == ':') {
+            return "Neither";
+        }
+        String[] xs = queryIP.split("[.]");
+        if (xs.length == 4) {
+            if (checkIPv4(xs)) {
+                return "IPv4";
+            } else {
+                return "Neither";
+            }
+        } else {
+            String[] ys = queryIP.split(":");
+            if (ys.length == 8) {
+                if (checkIPv6(ys)) {
+                    return "IPv6";
+                } else {
+                    return "Neither";
+                }
+            } else {
+                return "Neither";
+            }
+        }
+    }
+}
