468a4edad4f38f4c2c658da6b72f6802f25e8d20
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2023-01-23 09:34:18 +0800

    Time: 13 ms (69.45%), Space: 42.2 MB (51.77%) - LeetHub

--- 0259-3sum-smaller/0259-3sum-smaller.java
+++ 0259-3sum-smaller/0259-3sum-smaller.java
@@ -1,6 +1,7 @@
 class Solution {
     private int n;
     private Integer[] diffs;
+    private int[] nums;
     
     private int binarySearch(int target) {
         int l = 0, r = n - 1;
@@ -17,35 +18,58 @@ class Solution {
         return index;
     }
     
+    private int twoSumSmaller(int start, int target) {
+        int count = 0;
+        int l = start, r = n - 1;
+        while (l < r) {
+            if (nums[l] + nums[r] < target) {
+                count += (r - l);
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return count;
+    }
+    
     public int threeSumSmaller(int[] nums, int target) {
         n = nums.length;
         if (n == 0) {
             return 0;
         }
-        diffs = new Integer[n];
-        for (int i = 0; i < n; ++i) {
-            diffs[i] = target - nums[i];
-        }
-        Arrays.sort(diffs, Collections.reverseOrder());
+        this.nums = nums;
+        Arrays.sort(this.nums);
         
         int count = 0;
         for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                int x = nums[i] + nums[j];
-                int index = binarySearch(x);
-                if (index == -1) {
-                    continue;
-                }
-                count += (index + 1);
-                if (2 * nums[i] + nums[j] < target) {
-                    count--;
-                }
-                if (nums[i] + 2 * nums[j] < target) {
-                    count--;
-                }
-            }
+            count += twoSumSmaller(i + 1, target - nums[i]);
         }
-        count = (int) count / 3;
         return count;
+//         diffs = new Integer[n];
+//         for (int i = 0; i < n; ++i) {
+//             diffs[i] = target - nums[i];
+//         }
+//         Arrays.sort(diffs, Collections.reverseOrder());
+        
+//         int count = 0;
+//         for (int i = 0; i < n; ++i) {
+//             for (int j = i + 1; j < n; ++j) {
+//                 int x = nums[i] + nums[j];
+//                 int index = binarySearch(x);
+//                 if (index == -1) {
+//                     continue;
+//                 }
+//                 count += (index + 1);
+//                 if (2 * nums[i] + nums[j] < target) {
+//                     count--;
+//                 }
+//                 if (nums[i] + 2 * nums[j] < target) {
+//                     count--;
+//                 }
+//             }
+//         }
+//         count = (int) count / 3;
+//         return count;
+        
     }
 }
