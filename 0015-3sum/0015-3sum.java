d5b5df7216be3fad29d31207b244154f432c8c75
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2023-01-30 17:18:22 +0800

    Time: 657 ms (19.59%), Space: 235.1 MB (5.22%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,48 @@
+class Solution {
+    private List<List<Integer>> twoSum(int[] nums, int start, int target) {
+        int n = nums.length;
+        int l = start, r = n - 1;
+        List<List<Integer>> res = new ArrayList<>();
+        while (l < r) {
+            if (nums[l] + nums[r] < target) {
+                l++;
+            } else if (nums[l] + nums[r] == target) {
+                List<Integer> pair = new ArrayList<>();
+                pair.add(nums[l]);
+                pair.add(nums[r]);
+                res.add(pair);
+                l++;
+                r--;
+            } else {
+                r--;
+            }
+        }
+        return res;
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        List<List<Integer>> tempAns = new ArrayList<>();
+        for (int i = 0; i < n - 1; ++i) {
+            int target = -nums[i];
+            List<Integer> triplet;
+            for (List pair: twoSum(nums, i + 1, target)) {
+                triplet = new ArrayList<>();
+                triplet.add(-target);
+                triplet.addAll(pair);
+                tempAns.add(triplet);
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        HashMap<List<Integer>, Boolean> existed = new HashMap<>();
+        for (List triplet: tempAns) {
+            if (!existed.containsKey(triplet)) {
+                existed.put(triplet, true);
+                ans.add(triplet);
+            }
+        }
+        return ans;
+    }
+}
