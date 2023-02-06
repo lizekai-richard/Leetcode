f8129b2650592bba81dae9545b7ea45f1f351ba2
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2023-02-06 18:56:41 +0800

    Time: 139 ms (42.22%), Space: 129.2 MB (51.11%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.java
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.java
@@ -1,74 +1,71 @@
 class Solution {
-    private boolean[] visited;
+    private int n;
+    private HashMap<Integer, List<Integer>> adjList;
+    private int[] nodeColor;
     private int[][] dp;
-    private List<Integer>[] adj;
-    public int largestPathValue(String colors, int[][] edges) {
-        if(edges.length == 0) 
-            return 1;
-        
-        adj = new List[colors.length()];
-        for(int[] edge: edges) {
-            if(adj[edge[0]] == null) {
-                adj[edge[0]] = new ArrayList<>();
-            }
-            
-            adj[edge[0]].add(edge[1]);
-        }
-        
-        visited = new boolean[colors.length()];
-        dp = new int[colors.length()][26]; 
-        
-        int max = 0;
-        for(int i = 0; i < colors.length(); i++) {
-            if(!visited[i]) {
-                if(dfs(colors, new boolean[colors.length()], i)) {
-                    return -1;
-                }
-                
-                // dp[i][c] = max number of times we see color c
-                // in any given path starting from node i
-                for(int c = 0; c < 26; c++) {
-                    max = Math.max(dp[i][c], max);
-                }
-            }
-        }
-        
-        return max;
-    }
+    private boolean[] inStack;
+    private boolean[] visited;
     
-    // return true if cycle detected.
-    private boolean dfs(
-        String colors, 
-        boolean[] inStack,
-        int node
-    ) {
-        
-        if(inStack[node]) {
+    private boolean dfs(int u) {
+        if (inStack[u]) {
             return true;
         }
-        
-        if(visited[node]) {
+        if (visited[u]) {
             return false;
         }
-        
-        inStack[node] = true;
-        visited[node] = true;
-        
-        if(adj[node] != null) {
-            for(int nbr: adj[node]) {
-                if(dfs(colors, inStack, nbr)) {
+        visited[u] = true;
+        inStack[u] = true;
+        if (adjList.containsKey(u)) {
+            List<Integer> neighbors = adjList.get(u);
+            for (int v: neighbors) {
+                if (dfs(v)) {
                     return true;
                 }
-
-                for(int c = 0; c < 26; c++) {
-                    dp[node][c] = Math.max(dp[node][c], dp[nbr][c]);
+                for (int c = 0; c < 26; ++c) {
+                    dp[u][c] = Math.max(dp[u][c], dp[v][c]);
+                }
+            }
+        }
+        dp[u][nodeColor[u]]++;
+        inStack[u] = false;
+        return false;
+    }
+    
+    public int largestPathValue(String colors, int[][] edges) {
+        adjList = new HashMap<>();
+        n = colors.length();
+        for (int[] edge: edges) {
+            int u = edge[0], v = edge[1];
+            if (!adjList.containsKey(u)) {
+                adjList.put(u, new ArrayList<Integer>());
+            }
+            adjList.get(u).add(v);
+        }
+        nodeColor = new int[n];
+        for (int i = 0; i < colors.length(); ++i) {
+            nodeColor[i] = (int)colors.charAt(i) - (int)'a';
+        }
+        
+        dp = new int[n][26];
+        visited = new boolean[n];
+        inStack = new boolean[n];
+        Arrays.fill(visited, false);
+        
+        for (int i = 0; i < n; ++i){
+            if (!visited[i]) {
+                Arrays.fill(inStack, false);
+                if(dfs(i)) {
+                    return -1;
                 }
             }
         }
         
-        dp[node][colors.charAt(node) - 'a']++;
-        inStack[node] = false;
-        
-        return false;
-    }    
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < 26; ++j) {
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+        return ans;
+    }
 }
