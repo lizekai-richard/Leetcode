6f7f58e8105f7e1c875854daa949e3a477fe3b6f
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2023-05-19 18:01:18 +0800

    Time: 2 ms (94.86%), Space: 41.6 MB (99.59%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -3,21 +3,19 @@ class Solution {
         if (s.isBlank()) {
             return 0;
         }
-        Stack<Integer> stack = new Stack<>();
-        int n = s.length();
         int ans = 0;
-        stack.push(-1);
-        for (int i = 0; i < n; ++i) {
-            if (s.charAt(i) == '(') {
-                stack.push(i);
-            } else {
-                stack.pop();
-                if (!stack.isEmpty()) {
-                    ans = Math.max(ans, i - stack.peek());
-                } else {
-                    stack.push(i);
+        int n = s.length();
+        int[] dp = new int[n];
+        for (int i = 1; i < n; ++i) {
+            if (s.charAt(i) == '(') dp[i] = 0;
+            else {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = i >= 2 ? dp[i - 2] + 2 : 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + (i - dp[i - 1] - 2 >= 0 ? dp[i - dp[i - 1] - 2] + 2 : 2);
                 }
             }
+            ans = Math.max(ans, dp[i]);
         }
         return ans;
     }
